e75ec152e7e4e26c972776739faa1f666bb91b75
antagonistry <antagonistry@protonmail.com>
2026-03-02 03:55:41 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,78 @@
+# magnet - Minimalistic build system
+magnet is an interpreted minimal build language made by @antagonistry and is
+blazingly fast and extremely light-weighted, it also only have 8 keywords
+to remember!
+
+## Keywords
+- __'#':__ does nothing
+- __'!':__ create task that check for error and stop the script if found error
+- __'@':__ create task that check for error
+- __'$':__ create task that check for error without stopping the script
+- __'%':__ create task that doesn't check for any error
+- __'^':__ create task that only run if there is any error inside the previous task
+- __'/':__ create task that only run if there isn't any error inside the previous task
+- __':':__ run command that will return error if found
+- __'-':__ run command that won't return any error
+
+## Safety
+magnet is considered to be unsafe build language, cause it can cause fork bomb
+by the shell, but magnet can also be considered as a safe language when only
+looking at the interpreter.
+
+## Examples
+Here's an example build script that would output "Hello, World!".
+
+```sh
+!
+    :echo "Hello, World!"
+```
+
+Yeah, is it that easy? just two line of code and we're done!
+Also here's another example for opening a firefox tab.
+
+```sh
+!firefox
+    :example.com
+```
+
+If you want to add a little bit of flavor and use the conditional
+keyword like `/` and `^`, see this!
+
+```sh
+$not_a_command
+    :arg
+
+    /
+        -echo "Execution successful."
+    ^
+        -echo "Execution failed."
+```
+
+You may now see this like an object-oriented language but don't worry,
+in reality, it's just a 1 dimensional language, meaning that the language
+itself has no depth, look at this script after it's been parsed.
+
+```sh
+$not_a_command
+:arg
+
+/
+-echo "Execution successful."
+^
+-echo "Execution failed."
+```
+
+Let's come to the fun part, becareful, this is a fork bomb!
+
+```sh
+# This script's named fork.mg
+
+!magnet
+    -fork.mg
+```
+
+What this code does is that it spawns version of itself again and again until
+there's an error and it'll eventually force quit itself, unlike the fork bomb
+in shell.
+
+## End
---
+++ main.c
@@ -0,0 +1,456 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+FILE *stream = NULL;
+int task_type = 0;
+int doing_task = 0;
+char task[256];
+char cmd[512];
+int len_task = 0;
+int ret = 0;
+int backup = 0;
+
+void preprocess(char line[], int len) {
+  switch (line[len - 1])
+  {
+    case '\n':
+      line[--len] = '\0';
+      break;
+    default:
+      break;
+  }
+
+  char *ptr = line;
+
+  while (true)
+  {
+    switch (*ptr)
+    {
+      case ' ':
+      case '\t':
+        ptr++, len--;
+        continue;
+    }
+
+    break;
+  }
+
+  while (true)
+  {
+    switch (ptr[len - 1])
+    {
+      case ' ':
+      case '\t':
+        ptr[--len] = '\0';
+        continue;
+    }
+
+    break;
+  }
+
+  memcpy(line, ptr, len + 1);
+  return;
+}
+
+void parse(char *ptr, int len) {
+  switch (*ptr)
+  {
+    case '\0':
+    case '#':
+      return;
+    case ':':
+      switch (doing_task)
+      {
+        case 0:
+          fputs(ptr, stderr);
+          puts(" >> not inside task");
+          fclose(stream);
+          exit(EXIT_FAILURE);
+        case 1:
+          break;
+      }
+
+      switch (task_type)
+      {
+        case 0:
+        case 1:
+        case 2:
+          memcpy(cmd, task, len_task + 1);
+          strncat(cmd, " ", 2);
+          strncat(cmd, ++ptr, len);
+          
+          switch (strlen(cmd))
+          {
+            case sizeof(cmd):
+              fputs(ptr, stderr);
+              puts(" >> command too long");
+              fclose(stream);
+              exit(EXIT_FAILURE);
+              break;
+            default:
+              break;
+          }
+
+          ret = system(cmd);
+          break;
+        case 3:
+          switch (ret)
+          {
+            case 0:
+              break;
+            default:
+              return;
+          }
+
+          memcpy(cmd, task, len_task + 1);
+          strncat(cmd, " ", 2);
+          strncat(cmd, ++ptr, len);
+          
+          switch (strlen(cmd))
+          {
+            case sizeof(cmd):
+              fputs(ptr, stderr);
+              puts(" >> command too long");
+              fclose(stream);
+              exit(EXIT_FAILURE);
+              break;
+            default:
+              break;
+          }
+
+          ret = system(cmd);
+          break;
+        case 4:
+          switch (ret)
+          {
+            case 0:
+              return;
+            default:
+              break;
+          }
+
+          memcpy(cmd, task, len_task + 1);
+          strncat(cmd, " ", 2);
+          strncat(cmd, ++ptr, len);
+          
+          switch (strlen(cmd))
+          {
+            case sizeof(cmd):
+              fputs(ptr, stderr);
+              puts(" >> command too long");
+              fclose(stream);
+              exit(EXIT_FAILURE);
+              break;
+            default:
+              break;
+          }
+
+          ret = system(cmd);
+          break;
+        case 5:
+          memcpy(cmd, task, len_task + 1);
+          strncat(cmd, " ", 2);
+          strncat(cmd, ++ptr, len);
+          strncat(cmd, " > /dev/null", 13);
+
+          switch (strlen(cmd))
+          {
+            case sizeof(cmd):
+              fputs(ptr, stderr);
+              puts(" >> command too long");
+              fclose(stream);
+              exit(EXIT_FAILURE);
+              break;
+            default:
+              break;
+          }
+
+          ret = system(cmd);
+          break;
+      }
+
+      switch (task_type)
+      {
+        case 0:
+        case 5:
+          break;
+        case 1:
+          switch (ret)
+          {
+            case 0:
+              break;
+            case 1:
+              fputs(cmd, stderr);
+              puts(" >> exit failure");
+              fclose(stream);
+              exit(ret);
+              break;
+            case 127:
+              fputs(cmd, stderr);
+              puts(" >> command not found");
+              fclose(stream);
+              exit(ret);
+              break;
+            case 130:
+              fputs(cmd, stderr);
+              puts(" >> interrupt");
+              fclose(stream);
+              exit(ret);
+              break;
+            case 139:
+              fputs(cmd, stderr);
+              puts(" >> segmentation fault");
+              fclose(stream);
+              exit(ret);
+              break;
+            case 32512:
+              fputs(cmd, stderr);
+              puts(" >> force quit");
+              fclose(stream);
+              exit(ret);
+              break;
+            default:
+              fputs(cmd, stderr);
+              puts(" >> other error");
+              fclose(stream);
+              exit(ret);
+              break;
+          }
+
+          break;
+        case 2:
+          switch (ret)
+          {
+            case 0:
+              break;
+            case 1:
+              fputs(cmd, stderr);
+              puts(" >> exit failure");
+              break;
+            case 127:
+              fputs(cmd, stderr);
+              puts(" >> command not found");
+              break;
+            case 130:
+              fputs(cmd, stderr);
+              puts(" >> interrupt");
+              break;
+            case 139:
+              fputs(cmd, stderr);
+              puts(" >> segfault");
+              break;
+            case 32512:
+              fputs(cmd, stderr);
+              puts(" >> force quit");
+              fclose(stream);
+              exit(ret);
+              break;
+            default:
+              fputs(cmd, stderr);
+              puts(" >> other error");
+              break;
+          }
+
+          break;
+      }
+
+      break;
+    case '@':
+      task_type = 0;
+      doing_task = 1;
+      memcpy(task, ++ptr, len);
+      len_task = strlen(task);
+      break;
+    case '!':
+      task_type = 1;
+      doing_task = 1;
+      memcpy(task, ++ptr, len);
+      len_task = strlen(task);
+      break;
+    case '-':
+      switch (doing_task)
+      {
+        case 0:
+          fputs(ptr, stderr);
+          puts(" >> not inside task");
+          fclose(stream);
+          exit(EXIT_FAILURE);
+        case 1:
+          break;
+      }
+
+      switch (task_type)
+      {
+        case 0:
+        case 1:
+        case 2:
+          memcpy(cmd, task, len_task + 1);
+          strncat(cmd, " ", 2);
+          strncat(cmd, ++ptr, len);
+          
+          switch (strlen(cmd))
+          {
+            case sizeof(cmd):
+              fputs(ptr, stderr);
+              puts(" >> command too long");
+              fclose(stream);
+              exit(EXIT_FAILURE);
+              break;
+            default:
+              break;
+          }
+
+          backup = ret;
+          ret = system(cmd);
+          break;
+        case 3:
+          switch (ret)
+          {
+            case 0:
+              break;
+            default:
+              return;
+          }
+
+          memcpy(cmd, task, len_task + 1);
+          strncat(cmd, " ", 2);
+          strncat(cmd, ++ptr, len);
+          
+          switch (strlen(cmd))
+          {
+            case sizeof(cmd):
+              fputs(ptr, stderr);
+              puts(" >> command too long");
+              fclose(stream);
+              exit(EXIT_FAILURE);
+              break;
+            default:
+              break;
+          }
+
+          backup = ret;
+          ret = system(cmd);
+          break;
+        case 4:
+          switch (ret)
+          {
+            case 0:
+              return;
+            default:
+              break;
+          }
+
+          memcpy(cmd, task, len_task + 1);
+          strncat(cmd, " ", 2);
+          strncat(cmd, ++ptr, len);
+
+          switch (strlen(cmd))
+          {
+            case sizeof(cmd):
+              fputs(ptr, stderr);
+              puts(" >> command too long");
+              fclose(stream);
+              exit(EXIT_FAILURE);
+              break;
+            default:
+              break;
+          }
+
+          backup = ret;
+          ret = system(cmd);
+          break;
+        case 5:
+          memcpy(cmd, task, len_task + 1);
+          strncat(cmd, " ", 2);
+          strncat(cmd, ++ptr, len);
+          strncat(cmd, " > /dev/null", 13);
+
+          switch (strlen(cmd))
+          {
+            case sizeof(cmd):
+              fputs(ptr, stderr);
+              puts(" >> command too long");
+              fclose(stream);
+              exit(EXIT_FAILURE);
+              break;
+            default:
+              break;
+          }
+
+          backup = ret;
+          ret = system(cmd);
+          break;
+      }
+
+      switch (ret)
+      {
+        case 32512:
+          fputs(cmd, stderr);
+          puts(" >> force quit");
+          fclose(stream);
+          exit(ret);
+          break;
+        default:
+          break;
+      }
+
+      ret = backup;
+      break;
+    case '$':
+      task_type = 2;
+      doing_task = 1;
+      memcpy(task, ++ptr, len);
+      len_task = strlen(task);
+      break;
+    case '/':
+      task_type = 3;
+      doing_task = 1;
+      memcpy(task, ++ptr, len);
+      len_task = strlen(task);
+      break;
+    case '^':
+      task_type = 4;
+      doing_task = 1;
+      memcpy(task, ++ptr, len);
+      len_task = strlen(task);
+      break;
+    case '%':
+      task_type = 5;
+      doing_task = 1;
+      memcpy(task, ++ptr, len);
+      len_task = strlen(task);
+      break;
+    default:
+      fputs(ptr, stderr);
+      puts(" >> unknown command");
+      fclose(stream);
+      exit(EXIT_FAILURE);
+      return;
+  }
+
+  return;
+}
+
+int main(int argc, char **argv) {
+  char line[256];
+  
+  while (*++argv)
+  {
+    stream = fopen(*argv, "rb");
+
+    if (!stream)
+    { continue; }
+
+    while (fgets(line, sizeof(line), stream))
+    {
+      int len = strlen(line);
+      preprocess(line, len);
+      parse(line, len);
+    }
+
+    fclose(stream);
+    continue;
+  }
+  return EXIT_SUCCESS;
+}
